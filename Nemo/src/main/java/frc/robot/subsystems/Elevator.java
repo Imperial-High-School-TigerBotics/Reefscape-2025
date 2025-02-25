d5ae99135c0ff0c3267d21d654c7be4951649828
Mando8119 <armando.g.barron1@gmail.com>
2025-02-24 21:05:32 -0800

    try this

--- Nemo/src/main/java/frc/robot/subsystems/Elevator.java
+++ Nemo/src/main/java/frc/robot/subsystems/Elevator.java
@@ -119,7 +119,7 @@ public class Elevator extends SubsystemBase {
     }
 
     public void elevatorMove(double speed){
-        elevatorMotor1.set(speed * Constants.ElevatorConstants.elevatorMotor1speed);
+        elevatorMotor1.set(-speed * Constants.ElevatorConstants.elevatorMotor1speed);
         elevatorMotor2.set(speed * Constants.ElevatorConstants.elevatorMotor2speed);
     }
 
