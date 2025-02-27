f296802fe1b83bfe5053181e7d474a3bb59592fd
Mando8119 <armando.g.barron1@gmail.com>
2025-02-26 22:31:06 -0800

    automated movement test

--- Nemo/src/main/java/frc/robot/commands/ElevatorCmd.java
+++ Nemo/src/main/java/frc/robot/commands/ElevatorCmd.java
@@ -31,19 +31,26 @@ public class ElevatorCmd extends Command {
 
     @Override 
     public void execute() {
+
         if (DriverStation.isTeleop()) {
-           if (!autoShooter) {
+
+            if(xbox.getXButtonPressed()){
+                elevator.setElevatorPosition(Constants.ElevatorConstants.max_elevator_pos / 2);
+            }
+
+            if (!autoShooter) {
                 double axis = -MathUtil.applyDeadband(xbox.getRawAxis(1), Constants.stickDeadband);
                 if (axis != 0) {
                     elevatorPos = MathUtil.clamp(
-                        elevator.getElevatorCoderPos() + (axis * Constants.ElevatorConstants.axis_multiplier),
-                        Constants.ElevatorConstants.min_elevator_pos,
-                        Constants.ElevatorConstants.max_elevator_pos
+                    elevator.getElevatorCoderPos() + (axis * Constants.ElevatorConstants.axis_multiplier),
+                    Constants.ElevatorConstants.min_elevator_pos,
+                    Constants.ElevatorConstants.max_elevator_pos
                     );
                     elevator.setElevatorPosition(elevatorPos);
                 } else {
                     elevator.setElevatorPosition(elevatorPos); // Maintain last position
                 }
+
             }
         }
     }
