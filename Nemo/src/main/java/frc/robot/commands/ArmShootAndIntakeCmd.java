a6dc3d2b92cf3d0e2886bf9ac30d705a9d564d6f
be4ryllium <uhavee00@gmail.com>
2025-02-26 23:48:46 -0800

    Mandos Commit: Updated ArmShootAndIntakeCmd.java

--- Nemo/src/main/java/frc/robot/commands/ArmShootAndIntakeCmd.java
+++ Nemo/src/main/java/frc/robot/commands/ArmShootAndIntakeCmd.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 
 public class ArmShootAndIntakeCmd extends Command{
 
@@ -22,39 +23,20 @@ public class ArmShootAndIntakeCmd extends Command{
 
     @Override
     public void execute() {
-    arm.updateDashboard();
-    if (DriverStation.isTeleop()) {
-            // Ball Intake Controls
-             if (xbox.getYButtonPressed()) {
-                 arm.BallIntakeIn();
-             } 
-             if (xbox.getYButtonReleased()) {
-                 arm.BallIntakeStop();
-             }
+        arm.updateDashboard();
+        if (DriverStation.isTeleop()) {
+            //Coral Intake
+            if(xbox.getLeftBumperButtonPressed()){
+                arm.CoralIntakeIn();
+            }else if(xbox.getRightBumperButtonPressed()){
+                arm.CoralIntakeOut();
+            }else{arm.CoralIntakeStop();}
 
-             if (xbox.getBButtonPressed()) {
-                 arm.BallIntakeOut();
-             } 
-             if (xbox.getBButtonReleased()) {
-                 arm.BallIntakeStop();
-             }
-
-             // Coral Intake Controls
-             if (xbox.getXButtonPressed()) {
-                 arm.CoralIntakeIn();
-             } 
-             if (xbox.getXButtonReleased()) {
-                 arm.CoralIntakeStop();
-             }
-
-             if (xbox.getAButtonPressed()) {
-                 arm.CoralIntakeOut();
-             } 
-             if (xbox.getAButtonReleased()) {
-                 arm.CoralIntakeStop();
-             }
-            }
+            if(arm.getBumperButtonPressed(xbox.getLeftTriggerAxis())){
+                arm.BallIntakeIn();
+            }else if(arm.getBumperButtonPressed(xbox.getRightTriggerAxis())){
+                arm.BallIntakeOut();
+            }else{arm.BallIntakeStop();}
         }
-
+    }
 }
-
