40af653506ccadff2bf774c555787f81d55eb46a
Mando8119 <armando.g.barron1@gmail.com>
2025-02-27 01:14:05 -0800

    updated axis multiplier

--- Nemo/src/main/java/frc/robot/Constants.java
+++ Nemo/src/main/java/frc/robot/Constants.java
@@ -216,7 +216,7 @@ public final class Constants {
     public static final double elevatorSafety = 0.0;
     public static final double elevatorLimitSwitchOffset = 10;
 
-    public static final double axis_multiplier  = 0.09;
+    public static final double axis_multiplier  = 0.18;
 
     public static final double min_elevator_pos = 0.0;
     public static final double max_elevator_pos = 8.4 - elevatorSafety;
