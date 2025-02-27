3d7e4a8f9a02dbfff6c326419c9905a2ce268276
be4ryllium <uhavee00@gmail.com>
2025-02-27 00:03:49 -0800

    Updated

--- Nemo/src/main/java/frc/robot/Constants.java
+++ Nemo/src/main/java/frc/robot/Constants.java
@@ -35,6 +35,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final int shooterController = 1;
+
+    public static final double BUMPERTHRESHOLD = 0.1; 
   }
 
 
