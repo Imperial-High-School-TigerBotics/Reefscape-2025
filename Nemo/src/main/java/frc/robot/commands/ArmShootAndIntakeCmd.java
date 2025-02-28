2dcc6ed75f70a8a7a5f2be8247d3127d0d9cdb25
Mando8119 <armando.g.barron1@gmail.com>
2025-02-27 17:51:50 -0800

    Fixed controls for intake / outtake

--- Nemo/src/main/java/frc/robot/commands/ArmShootAndIntakeCmd.java
+++ Nemo/src/main/java/frc/robot/commands/ArmShootAndIntakeCmd.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import frc.robot.Constants;
 import frc.robot.subsystems.ArmShootAndIntake;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
@@ -22,39 +23,32 @@ public class ArmShootAndIntakeCmd extends Command{
 
     @Override
     public void execute() {
-    arm.updateDashboard();
-    if (DriverStation.isTeleop()) {
-            // Ball Intake Controls
-             if (xbox.getYButtonPressed()) {
-                 arm.BallIntakeIn();
-             } 
-             if (xbox.getYButtonReleased()) {
-                 arm.BallIntakeStop();
-             }
+        arm.updateDashboard();
+        if (DriverStation.isTeleop()) {
+            boolean ltPressed = xbox.getLeftTriggerAxis() > Constants.OperatorConstants.TRIGGER_THRESHOLD;
+            boolean rtPressed = xbox.getRightTriggerAxis() > Constants.OperatorConstants.TRIGGER_THRESHOLD;
+            boolean lbPressed = xbox.getLeftBumperButton();
+            boolean rbPressed = xbox.getRightBumperButton();
 
-             if (xbox.getBButtonPressed()) {
-                 arm.BallIntakeOut();
-             } 
-             if (xbox.getBButtonReleased()) {
-                 arm.BallIntakeStop();
-             }
 
-             // Coral Intake Controls
-             if (xbox.getXButtonPressed()) {
-                 arm.CoralIntakeIn();
-             } 
-             if (xbox.getXButtonReleased()) {
-                 arm.CoralIntakeStop();
-             }
-
-             if (xbox.getAButtonPressed()) {
-                 arm.CoralIntakeOut();
-             } 
-             if (xbox.getAButtonReleased()) {
-                 arm.CoralIntakeStop();
-             }
+            // Intake / Outtake Coral
+            if(ltPressed){
+                arm.CoralIntakeIn();
+            }else if(rtPressed){
+                arm.CoralIntakeOut();
+            }else{
+                arm.CoralIntakeStop();
             }
+
+            // Intake / Outtake Ball
+            if(lbPressed){
+                arm.BallIntakeIn();
+            }else if(rbPressed){
+                arm.BallIntakeOut();
+            }else{
+                arm.BallIntakeStop();
+            }
+
         }
-
+    }
 }
-
