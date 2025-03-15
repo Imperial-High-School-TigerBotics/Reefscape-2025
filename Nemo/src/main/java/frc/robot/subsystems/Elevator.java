962fd21c946c202c6cfd6724253ca9482583f357
Nathan S. <72057664+Pengalu@users.noreply.github.com>
2025-03-15 11:58:12 -0700

    Added dynamic (?) PID variable

--- Nemo/src/main/java/frc/robot/subsystems/Elevator.java
+++ Nemo/src/main/java/frc/robot/subsystems/Elevator.java
@@ -23,11 +23,13 @@ public class Elevator extends SubsystemBase {
 
     private DigitalInput limitSwitchTop = new DigitalInput(Constants.ElevatorConstants.limitSwitchTop);
     private DigitalInput limitSwitchBottom = new DigitalInput(Constants.ElevatorConstants.limitSwitchBottom);
-
+    
     public double ElevatorPos;
 
     public Elevator() {
         // Initialize motors with brake mode
+        SmartDashboard.setDefaultNumber("p term",   Constants.ElevatorConstants.elevatorP );
+        
         elevatorMotor1 = new TalonFX(Constants.ElevatorConstants.elevatorMotor1ID);
         elevatorMotor1.setNeutralMode(NeutralModeValue.Brake);
         elevatorMotor1PID = new PIDController(
@@ -45,8 +47,7 @@ public class Elevator extends SubsystemBase {
         );
         elevatorCoder = new CANcoder(Constants.ElevatorConstants.elevatorCoderID);
 
-        NetworkTable = NetworkTable.getInstance()
-        NetworkTable table = Robot.getInstance();
+        //NetworkTable table = Robot.getInstance();
 
         ElevatorPos = Constants.ElevatorConstants.min_elevator_pos;
     }
@@ -219,6 +220,9 @@ public class Elevator extends SubsystemBase {
 
         SmartDashboard.putNumber("Left Elevator Motor RPM", leftElevatorMotor1RPM());
         SmartDashboard.putNumber("Right Elevator Motor RPM", rightElevatorMotor2RPM());
+        double pTerm = SmartDashboard.getNumber("p term");
+        
+        
         nextElevatorPID();
     }
 
